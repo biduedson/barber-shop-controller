b90312fb361976ddcc49c4599347db8de5783a22
Edson Gomes <biduzao.bidu21@gmail.com>
2024-02-02 20:29:27 -0300

    feat: cinfugure initial header

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Header from "../_components/header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h2>Olá, Miguel!</h2>
+    </div>
+  );
+}
